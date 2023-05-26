e0d43bf95209b4610f35bd7102d9c332000e3847
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-05-26 10:56:34 +0700

    Initial commit

---
+++ apps/psuservice-monorepo/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'psuservice-monorepo',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/psuservice-monorepo',
+};
