d93f9158ac6fb5458d20c2f0b5088e24a6340660
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-18 17:52:44 +0700

    improve: add next auth remap response

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,30 +1,39 @@
-import { Body, Controller, Post, HttpCode, HttpStatus, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guards/index';
 @Controller('users')
 export class UsersController {
-  constructor(private userService:UsersService) {}
+  constructor(private userService: UsersService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('register')
-  register(@Body() registerDto: RegisterDto){
-    const result = this.userService.create(
-      {
-        email: registerDto.email,
-        password: registerDto.password,
-        name: registerDto.name
-      }
-    )
+  register(@Body() registerDto: RegisterDto) {
+    const result = this.userService.create({
+      email: registerDto.email,
+      password: registerDto.password,
+      name: registerDto.name,
+    });
 
-    return result
+    return result.then((user) => ({
+      name: user.name,
+      email: user.email,
+    }));
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('detail')
-  getDetail(@Request() req){
-    const user = req.user
-    return this.userService.profile(user.email)
+  getDetail(@Request() req) {
+    const user = req.user;
+    return this.userService.profile(user.email);
   }
-
 }
